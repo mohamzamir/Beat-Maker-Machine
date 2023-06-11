816dd696b69afab2829b51a3e3b835b59f087c08
mohamzamir <91626899+mohamzamir@users.noreply.github.com>
2023-06-11 18:37:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const nextClueWaitTime = 1000; //how long to wait before starting playback of th
 
 //Global Variables
 //var pattern = [2, 2, 4, 3, 6, 1, 2, 5];
-var pattern = [];
+var pattern = [1, 7, 6, 3, 3, 6, 7, 1, 7, 6, 3, 3, 6, 7];
 var progress = 0;
 var gamePlaying = false;
 var tonePlaying = false;
@@ -13,19 +13,120 @@ var volume = 0.5;
 var guessCounter = 0;
 var numberOfMistakes;
 
-function startGame() {
-  //initialize game variables
-  ranPattern();
-  console.log(pattern);
-  numberOfMistakes = 3;
-  progress = 0;
-  gamePlaying = true;
+const MODE_EASY = 'easy';
+const MODE_MEDIUM = 'medium';
+const MODE_HARD = 'hard';
 
-  // swap the Start and Stop buttons
-  document.getElementById("startBtn").classList.add("hidden");
-  document.getElementById("stopBtn").classList.remove("hidden");
-  playClueSequence();
+let mode = MODE_EASY; // Default mode
+let speedIncrement = 20; // Speed increment for each mode
+let easySpeed, mediumSpeed, hardSpeed;
+let score = 0;
+
+function changeMode(selectedMode) {
+  // Remove active class from all mode buttons
+  document.getElementById("easyModeBtn").classList.remove("active");
+  document.getElementById("mediumModeBtn").classList.remove("active");
+  document.getElementById("hardModeBtn").classList.remove("active");
+
+  // Add active class to the selected mode button
+  document.getElementById(`${selectedMode}ModeBtn`).classList.add("active");
+
+  // Update the mode variable
+  mode = selectedMode;
+
+  // Update button colors based on the selected mode
+  updateButtonColors();
 }
+
+function calculateSpeeds() {
+  // Calculate speed values for each mode
+  easySpeed = speedIncrement;
+  mediumSpeed = speedIncrement * 2;
+  hardSpeed = speedIncrement * 3;
+}
+
+function updateScoreDisplay() {
+  document.getElementById("score").textContent = score;
+}
+
+function incrementScore() {
+  score++;
+  updateScoreDisplay();
+}
+
+function updateButtonColors() {
+  const easyButton = document.getElementById("easyModeBtn");
+  const mediumButton = document.getElementById("mediumModeBtn");
+  const hardButton = document.getElementById("hardModeBtn");
+
+  // Update button colors based on the selected mode
+  const scoreDisplay = document.getElementById("score");
+
+  switch (mode) {
+    case MODE_EASY:
+      scoreDisplay.style.color = "#FFA500";
+      break;
+    case MODE_MEDIUM:
+      scoreDisplay.style.color = "#FFFFFF";
+      break;
+    case MODE_HARD:
+      scoreDisplay.style.color = "#FF0000";
+      break;
+  }
+
+  switch (mode) {
+    case MODE_EASY:
+      easyButton.style.backgroundColor = "#FFA500";
+      mediumButton.style.backgroundColor = "#FFFFFF";
+      hardButton.style.backgroundColor = "#FFFFFF";
+      break;
+    case MODE_MEDIUM:
+      easyButton.style.backgroundColor = "#FFFFFF";
+      mediumButton.style.backgroundColor = "#FFA500";
+      hardButton.style.backgroundColor = "#FFFFFF";
+      break;
+    case MODE_HARD:
+      easyButton.style.backgroundColor = "#FFFFFF";
+      mediumButton.style.backgroundColor = "#FFFFFF";
+      hardButton.style.backgroundColor = "#FFA500";
+      break;
+  }
+}
+
+function playSingleClue(btn) {
+  if (gamePlaying) {
+    lightButton(btn);
+    playTone(btn, clueHoldTime);
+    incrementScore(); // Increment the score when the button is played
+    setTimeout(clearButton, clueHoldTime, btn);
+  }
+}
+
+// Initial update of button colors on page load
+updateButtonColors();
+updateScoreDisplay();
+
+
+function startGame() {
+  calculateSpeeds();
+
+  score = 0;
+  updateScoreDisplay();
+
+  context.resume().then(() => {
+    ranPattern();
+    console.log(pattern);
+    numberOfMistakes = 3;
+    progress = 0;
+    gamePlaying = true;
+
+    // swap the Start and Stop buttons
+    document.getElementById("startBtn").classList.add("hidden");
+    document.getElementById("stopBtn").classList.remove("hidden");
+    playClueSequence();
+  });
+}
+
 
 function stopGame() {
   progress = 0;
@@ -35,6 +136,8 @@ function stopGame() {
   document.getElementById("startBtn").classList.remove("hidden");
   document.getElementById("stopBtn").classList.add("hidden");
   pattern = [];
+
+  updateScoreDisplay();
 }
 
 function lightButton(btn) {
@@ -67,11 +170,13 @@ function playClueSequence() {
 
 function loseGame() {
   stopGame();
+  updateScoreDisplay();
   alert("Game Over. You lost :( ");
 }
 
 function winGame() {
   stopGame();
+  score++;
   alert("Game Over. Congrats you learned how to make beats just like a dj!");
 }
 
@@ -106,6 +211,7 @@ function guess(btn) {
 
 // this func generates random sequence every game
 function ranPattern() {
+  pattern = [1, 7, 6, 3, 3, 6, 7, 1, 7, 6, 3, 3, 6, 7];
   for (let i = 0; i < 5; i++) {
     pattern.push(Math.floor(Math.random() * 8) + 1);
   }
@@ -113,24 +219,20 @@ function ranPattern() {
 
 // Sound Synthesis Functions
 const freqMap = {
-  1: 261.6,
-  2: 329.6,
-  3: 392,
-  4: 466.2,
-  5: 512.2,
-  6: 680,
-  7: 261.6,
-  8: 329.6,
-  9: 392,
-  10: 466.2,
-  11: 512.2,
-  12: 680
+  1: 261.63, // C
+  2: 293.66, // D
+  3: 329.63, // E
+  4: 349.23, // F
+  5: 392.00, // G
+  6: 440.00, // A
+  7: 493.88, // B
 };
+
 function playTone(btn, len) {
   o.frequency.value = freqMap[btn];
   g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
   tonePlaying = true;
-  setTimeout(function() {
+  setTimeout(function () {
     stopTone();
   }, len);
 }
